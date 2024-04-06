4885bab41722ef5f528794877c20353a80bafcc4
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2024-04-06 13:27:05 +0530

    Initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,24 @@
+import react from 'react';
+
+function App() {
+  return (
+    <>
+      <section>
+        <nav className="sidebar">
+          <h4>Softkingo</h4>
+         <div className="navbar">
+           <ul>
+             <li><a href="">Home</a></li>
+             <li><a href="">Home</a></li>
+             <li><a href="">Home</a></li>
+             <li><a href="">Home</a></li>
+             <li><a href="">Home</a></li>
+           </ul>
+         </div>
+        </nav>
+      </section>
+    </>
+  );
+}
+
+export default App;
